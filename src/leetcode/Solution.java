4278a09a791df3c1df4ddc038f91345315b7d1a3
junyeong-nero <magicwho@unist.ac.kr>
2021-04-16 21:01:22 +0900

    Solve : Solution.removeNthFromEnd

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -8,6 +8,25 @@ import java.util.List;
 
 public class Solution {
 
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode start = new ListNode(0);
+        ListNode slow = start, fast = start;
+        slow.next = head;
+
+        //Move fast in front so that the gap between slow and fast becomes n
+        for(int i = 0; i <= n; i++)   {
+            fast = fast.next;
+        }
+        //Move fast to the end, maintaining the gap
+        while(fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        //Skip the desired node
+        slow.next = slow.next.next;
+        return start.next;
+    }
+
     public ListNode swapPairs(ListNode head) {
         ListNode temp = head;
         while(temp != null && temp.next != null) {
