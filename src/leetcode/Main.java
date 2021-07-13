841436a32f0b64f600a7d8137ca7c5b351ff9349
junyeong-nero <77763121+junyeong-nero@users.noreply.github.com>
2021-07-13 10:08:29 +0900

    solve : Solution.isIsomorphic solve : Solution.isIsomorphic2 (one side version of Solution.isIsomorphic)

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -6,31 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[][] temp = new int[][]{
-                {1, 2},
-                {1, 2},
-                {1, 2},
-                {1, 2}
-        };
-        int[][] temp2 = new int[][]{
-                {1, 2},
-                {2, 3},
-                {3, 4},
-                {1, 3}
-        };
-
-//        sol.commitForRestDay(2020);
-//        System.out.println(sol.findLengthOfLCIS(new int[]{1, 3, 5, 4, 7}));
-//        System.out.println(sol.findLengthOfLCIS(new int[]{1, 3, 5, 7}));
-
-//        MedianFinder m = new MedianFinder();
-//        m.addNum(1);
-//        m.addNum(2);
-//        System.out.println(m.findMedian());
-//        m.addNum(3);
-//        System.out.println(m.findMedian());
-
-        System.out.println(sol.eraseOverlapIntervals(temp));
-        System.out.println(sol.eraseOverlapIntervals(temp2));
+//        System.out.println(sol.isIsomorphic("add", "egg"));
+//        System.out.println(sol.isIsomorphic("foo", "bar"));
+        System.out.println(sol.isIsomorphic("badc", "baba"));
     }
 }
--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -10,6 +10,28 @@ public class Solution {
         }
     }
 
+//    "badc"
+//    "baba"
+
+    public boolean isIsomorphic(String s, String t) {
+        return isIsomorphic2(s, t) && isIsomorphic2(t, s);
+    }
+
+    public boolean isIsomorphic2(String s, String t) {
+        HashMap<Character, Character> map = new HashMap<>();
+        int len = s.length();
+        for (int i = 0; i < len; ++i) {
+            char c = s.charAt(i);
+            char d = t.charAt(i);
+            if (map.containsKey(c)) {
+                if (map.get(c) != d) return false;
+            } else {
+                map.put(c, d);
+            }
+        }
+        return true;
+    }
+
     public int eraseOverlapIntervals(int[][] intervals) {
         if (intervals.length == 0)  return 0;
 
