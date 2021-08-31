19e11c4cbdc73f99a8cfb8323cd81ab98ed6c4da
junyeong-nero <magicwho@unist.ac.kr>
2021-08-31 17:45:18 +0900

    solve : Solution.findMin

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -4,6 +4,19 @@ import java.util.*;
 
 public class Solution {
 
+
+
+    public int findMin(int[] nums) {
+        return findMinDPS(nums, 0, nums.length - 1);
+    }
+
+    public int findMinDPS(int[] nums, int start, int end) {
+        if (start == end) return nums[start];
+        if (nums[start] < nums[end]) return nums[start];
+        int min = (start + end) / 2;
+        return Math.min(findMinDPS(nums, start, min), findMinDPS(nums, min + 1, end));
+    }
+
     Integer prev = null;
     int min = Integer.MAX_VALUE;
 
