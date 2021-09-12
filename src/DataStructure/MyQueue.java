b87795f9c1fc8fdce118b2d5980b85416a19d545
junyeong-nero <magicwho@unist.ac.kr>
2021-09-12 20:56:26 +0900

    [fix] MyQueue.java replace ArrayList<Integer> to int[]

--- src/DataStructure/MyQueue.java
+++ src/DataStructure/MyQueue.java
@@ -3,35 +3,36 @@ package DataStructure;
 import java.util.ArrayList;
 
 public class MyQueue {
-	ArrayList<Integer> arr = new ArrayList<>();
+	int[] arr;
 	int size = 0;
 	int front = 0;
 	int rear = 0;
 
 	public MyQueue(int capacity) {
 		this.size = capacity + 1;
-		for (int i = 0; i <= size; i++) arr.add(0);
+		this.arr = new int[size + 1];
+		for (int i = 0; i <= size; i++) arr[i] = 0;
 	}
 
 	public int get(int index) {
-		return arr.get((front + 1 + index) % size);
+		return arr[(front + 1 + index) % size];
 	}
 
 	public int pop() {
 		int res = this.get(0);
 		front++;
-		arr.set(front, 0);
+		arr[front] = 0;
 		return res;
 	}
 
 	public void push(int num) {
 		rear++;
-		arr.set(rear, num);
+		arr[rear] = num;
 	}
 
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		for (int i = front + 1; i <= rear; i++) builder.append(arr.get(i % size)).append(" ");
+		for (int i = front + 1; i <= rear; i++) builder.append(arr[i % size]).append(" ");
 		return builder.toString();
 	}
 }
