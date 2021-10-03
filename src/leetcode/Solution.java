372b152ac4c809b02d9df01873229a906ec0f352
junyeong-nero <magicwho@unist.ac.kr>
2021-10-03 17:25:58 +0900

    Added Solution.minCostClimbingStairs

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -8,7 +8,23 @@ import java.util.*;
 
 public class Solution {
 
-    p
+    public int minCostClimbingStairs(int[] cost) {
+        int len = cost.length;
+        // Corner case.
+        if (len == 0) return 0;
+        if (len == 1) return cost[0];
+
+        // Initialization.
+        int[] dp = new int[len];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < len; i++){
+            // Formula
+            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+        return Math.min(dp[len - 1], dp[len - 2]);
+    }
 
     public int climbStairsF(int n) {
         int[] dp = new int[n + 1];
