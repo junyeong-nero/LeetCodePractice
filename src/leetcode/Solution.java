7ddc62c48578f420cc9d635863929f74249c43da
junyeong-nero <magicwho@unist.ac.kr>
2021-10-12 16:47:41 +0900

    Solved 374. Guess Number Higher or Lower

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -8,5 +8,18 @@ import com.sun.source.tree.Tree;
 import java.util.*;
 
 public class Solution {
-
+	public int guessNumber(int n) {
+		int i = 1, j = n;
+		while(i < j) {
+			int mid = i + (j - i) / 2;
+			if(guess(mid) == 0) {
+				return mid;
+			} else if(guess(mid) == 1) {
+				i = mid + 1;
+			} else {
+				j = mid;
+			}
+		}
+		return i;
+	}
 }
