aeb36e9400bbf85de18a59e53bbddd2efdd51da2
junyeong-nero <magicwho@unist.ac.kr>
2022-02-14 13:14:19 +0900

    Solved #104. Maximum Depth of Binary Tree

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -15,6 +15,12 @@ public class Solution {
 
 	}
 
+	public int maxDepth(TreeNode root) {
+		if (root == null)
+			return 0;
+		return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+	}
+
 	List<List<Integer>> subsetList;
 	public List<List<Integer>> subsets(int[] nums) {
 		subsetList = new ArrayList<>();
