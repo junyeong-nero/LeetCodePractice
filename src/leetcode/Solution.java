13591a8041cb30018867bc89054ff686a274b380
junyeong-nero <magicwho@unist.ac.kr>
2022-03-11 11:31:56 +0900

    Solved minPathSums

--- src/leetcode/Solution.java
+++ src/leetcode/Solution.java
@@ -15,6 +15,22 @@ public class Solution {
 
 	}
 
+	public int minPathSum(int[][] grid) {
+		int m = grid.length;
+		int n = grid[0].length;
+		int[][] dp = new int[m][n];
+		dp[0][0] = grid[0][0];
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (i == 0 && j == 0)
+					continue;
+				dp[i][j] = Math.min(i > 0 ? dp[i - 1][j] : Integer.MAX_VALUE, j > 0 ? dp[i][j - 1] : Integer.MAX_VALUE) + grid[i][j];
+			}
+		}
+		System.out.println(Arrays.toString(dp));
+		return dp[m - 1][n - 1];
+	}
+
 	public int uniquePathsWithObstacles(int[][] obstacleGrid) {
 		int m = obstacleGrid.length;
 		int n = obstacleGrid[0].length;
